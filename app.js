8dbd48c51f8c5a5348952db7a7167c61402f9869
Alejandro Arias <josea.ariasp@uqvirtual.edu.co>
2023-02-13 18:58:34 -0500

    Method is working

---
+++ app.js
@@ -0,0 +1,30 @@
+console.log(isometricStrings("add", "egg")); // truea
+function isometricStrings(line1, line2) {
+    //creo una tabla hash para guardar las letras de la primera palabra
+    var hash = {};
+    //recorro la primera palabra y voy guardando las letras en la tabla hash
+    for (var i = 0; i < line1.length; i++) {
+        if (hash[line1[i]]) {
+            hash[line1[i]] += 1;
+        }
+        else {
+            hash[line1[i]] = 1;
+        }
+    }
+    //recorro la segunda palabra y voy restando las letras de la tabla hash
+    for (var i = 0; i < line2.length; i++) {
+        if (hash[line2[i]]) {
+            hash[line2[i]] -= 1;
+        }
+        else {
+            hash[line2[i]] = 1;
+        }
+    }
+    //recorro la tabla hash y si hay alguna letra que no se haya usado en la segunda palabra, devuelvo false
+    for (var key in hash) {
+        if (hash[key] !== 0) {
+            return false;
+        }
+    }
+    return true;
+}
